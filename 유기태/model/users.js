d3191fc12567281eacaa2d6fcb5412a1f31ba532
7IEH <80614927+7IEH@users.noreply.github.com>
2021-12-11 22:24:33 +0900

    heroku ready

--- 유기태/model/users.js
+++ 유기태/model/users.js
@@ -11,7 +11,7 @@ const userSchema = mongoose.Schema({
         unique:true,
         required:true
     },
-    Password:{
+    Password:{                  
         type:String,
         required:true
     }
